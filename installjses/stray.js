bb9f6185a4fb03bd0e3a18e70b606d7e6d837ed9
Oc-found <153354935+OcFounder@users.noreply.github.com>
2024-07-06 15:15:49 +0800

    a

---
+++ installjses/stray.js
@@ -0,0 +1,7 @@
+function tIjIAo() {
+  if (document.getElementById('JHmA').value != "CLstray"){
+    alert("兑换码不正确哦，如果你没有付费请尽快付费哦！");
+  }else{
+    window.location.href="https://pan.baidu.com/s/1c5BDJGud7oR3oUEcAux4tg";
+  }
+}
