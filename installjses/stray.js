b351e95d511a90e8912dd71252d7e71d2c75fd80
Oc-found <153354935+OcFounder@users.noreply.github.com>
2024-08-01 16:28:51 +0800

    a

--- installjses/stray.js
+++ installjses/stray.js
@@ -2,6 +2,6 @@ function tIjIAo() {
   if (document.getElementById('JHmA').value != "CLstray"){
     alert("兑换码不正确，如果你没有付费请尽快付费！");
   }else{
-    window.location.href="https://pan.baidu.com/s/1c5BDJGud7oR3oUEcAux4tg";
+    window.location.href="https://pan.baidu.com/s/1rNvCsKPfhTtI3BbZFRbLsw";
   }
 }
---
+++ installjses/superliminal.js
@@ -0,0 +1,7 @@
+function tIjIAo() {
+  if (document.getElementById('JHmA').value != "secondSuperliminal"){
+    alert("兑换码不正确，如果你没有付费请尽快付费！");
+  }else{
+    window.location.href="https://pan.baidu.com/s/1p1CZCnU73mudaEdv3xUthw";
+  }
+}
