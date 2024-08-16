efe6dd0e9fe02465bb44018686c0565542852a39
OcFounder <153354935+OcFounder@users.noreply.github.com>
2024-08-16 17:06:06 +0800

    更新 stray.js

--- installjses/stray.js
+++ installjses/stray.js
@@ -1,6 +1,6 @@
 function tIjIAo() {
-  if (document.getElementById('JHmA').value != "CLstray"){
-    alert("兑换码不正确，如果你没有付费请尽快付费！");
+  if (document.getElementById('JHmA').value != "CLSTR-AYQ9D-63JU0-QLMBD-AL8S5-HELLO-STRAY-16732"){
+    alert("无此兑换码");
   }else{
     window.location.href="https://pan.baidu.com/s/1rNvCsKPfhTtI3BbZFRbLsw";
   }
