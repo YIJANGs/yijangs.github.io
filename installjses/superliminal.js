42d790c94955545dba768068764ad75f9cee726a
OcFounder <153354935+OcFounder@users.noreply.github.com>
2024-08-16 17:08:41 +0800

    更新 superliminal.js

--- installjses/superliminal.js
+++ installjses/superliminal.js
@@ -1,6 +1,6 @@
 function tIjIAo() {
-  if (document.getElementById('JHmA').value != "secondSuperliminal"){
-    alert("兑换码不正确，如果你没有付费请尽快付费！");
+  if (document.getElementById('JHmA').value != "SECON-DSUPE-RLIMI-NAL00-4HSKD-SJZND-4H7SK-GOOD 6"){
+    alert("无此兑换码");
   }else{
     window.location.href="https://pan.baidu.com/s/1p1CZCnU73mudaEdv3xUthw";
   }
